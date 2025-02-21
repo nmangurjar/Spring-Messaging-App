67624949295a88702e670cebaf2285896730d620
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-21 16:23:58 +0530

    [Naman] Add. Make Rest Call by Path Variable

---
+++ SpringMessagingApp/src/main/java/com/hello/HelloController.java
@@ -0,0 +1,24 @@
+package com.hello;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/uc1")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+    @RequestMapping("/uc2")
+    public String sayHello(@RequestParam String name) {
+        return "Hello "+name+" from BridgeLabz";
+    }
+    @RequestMapping("/uc3/{name}")
+    public String sayHelloByPath(@PathVariable String name) {
+        return "Hello "+name+" from BridgeLabz";
+    }
+
+
+
+}
