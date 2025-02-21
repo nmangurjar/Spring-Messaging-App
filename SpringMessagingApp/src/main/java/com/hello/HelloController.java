a43c6b43785e754d51a32647108240cb314f43b1
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-21 21:09:42 +0530

    [Naman] Add.  Make Rest Call by Query Parameter. both Path Variable and

---
+++ SpringMessagingApp/src/main/java/com/hello/HelloController.java
@@ -0,0 +1,35 @@
+package com.hello;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/uc1")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+
+    @RequestMapping("/uc2")
+    public String sayHello(@RequestParam String name) {
+        return "Hello "+name+" from BridgeLabz";
+    }
+
+    @RequestMapping("/uc3/{name}")
+    public String sayHelloByPath(@PathVariable String name) {
+        return "Hello "+name+" from BridgeLabz";
+    }
+
+    @PostMapping("/uc4")
+    public String sayHello(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+        }
+        @RequestMapping("/uc5/{firstName}")
+        public String sayHello(@PathVariable String firstName ,@RequestParam String lastName){
+            return "Hello "+firstName+" "+lastName+" from BridgeLabz";
+        }
+    }
+
+
+
---
+++ SpringMessagingApp/src/main/java/com/hello/UserDTO.java
@@ -0,0 +1,31 @@
+package com.hello;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
